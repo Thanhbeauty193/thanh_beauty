946b19c8c5c83082c2707873426de5eed7c70cb7
tên <email>
2023-10-18 16:29:52 +0700

    okee

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,134 @@
+import "./style.css"
+export default function Home(){
+    return(
+        <div>
+          <div className="Blog">
+            <div className="header">
+            <div className="container">
+              <div className="i1">
+              <img src="./src/assets/image/1.jpg" alt="" className="a1a"></img>
+           <a className="a2a">Beautyness</a>
+           <img src="./src/assets/image/2.jpg" alt="" className="te"></img>
+          <a className="s2s">Call Us - (+22) 123 456 7890</a>
+              </div>
+              <div className="i2">
+              <a className="h1">Home</a>
+          <a className="h2">Pages</a>
+          <a className="h3">About</a>
+          <a className="h4">Services</a>
+          <a className="h5">Contact</a>
+              </div>
+          <div className="i3">
+            <img src="./src/assets/image/f.jpg" className="h1h"></img>
+          <img src="./src/assets/image/v.jpg" className="h2h"></img>
+          <img src="./src/assets/image/i.jpg" className="h3h"></img>
+          </div>
+          <button className="btt1" type="reset">RESERVATION</button>
+          
+          
+          </div>
+            </div>
+            <div className="nav">
+              <p className="na1">GET IN TOUCH WITH US</p>
+              <p className="na2">Articles & News</p>
+            </div>
+            <div className="Latest">
+              <div className="content1">
+                <div className="con1">
+                  <img className="im1" src="./src/assets/image/Im1.jpg"></img>
+                  <p className="g1">11TH AUGUST 2021--BY PETER BERGSON</p>
+                  <h1 className="g2">The energy efficiency of the hydrotherapy or swim</h1>
+                  <a className="g3">The point of using Lorem Ipsum hiter that using making it look like others readable will get end.</a>
+                </div>
+                <div className="con2">
+                <img className="im1" src="./src/assets/image/Im2.jpg"></img>
+                  <p className="g1">11TH AUGUST 2021--BY PETER BERGSON</p>
+                  <h1 className="g2">Tips to improve your body via cleansing</h1>
+                  <a className="g3">The point of using Lorem Ipsum hiter that using making it look like others readable will get end.</a>
+                </div>
+                <div className="con3">
+                <img className="im1" src="./src/assets/image/Im3.jpg"></img>
+                <p className="g1">11TH AUGUST 2021--BY PETER BERGSON</p>
+                  <h1 className="g2">Experience the beauty at our beauty spa</h1>
+                  <a className="g3">The point of using Lorem Ipsum hiter that using making it look like others readable will get end.</a>
+                </div>
+              </div>
+              <div className="content2">
+              <div className="con1">
+                  <img className="im1" src="./src/assets/image/Im4.jpg"></img>
+                  <p className="g1">11TH AUGUST 2021--BY PETER BERGSON</p>
+                  <h1 className="g2">The best and perfect skincare strategy</h1>
+                  <a className="g3">The point of using Lorem Ipsum hiter that using making it look like others readable will get end.</a>
+                </div>
+                <div className="con2">
+                <img className="im1" src="./src/assets/image/Im5.jpg"></img>
+                  <p className="g1">11TH AUGUST 2021--BY PETER BERGSON</p>
+                  <h1 className="g2">Fell the natural reverse aging process</h1>
+                  <a className="g3">The point of using Lorem Ipsum hiter that using making it look like others readable will get end.</a>
+                </div>
+                <div className="con3">
+                <img className="im1" src="./src/assets/image/Im6.jpg"></img>
+                <p className="g1">11TH AUGUST 2021--BY PETER BERGSON</p>
+                  <h1 className="g2">How to make the make-up stay last</h1>
+                  <a className="g3">The point of using Lorem Ipsum hiter that using making it look like others readable will get end.</a>
+                </div>
+              
+              </div>
+              <button className="btt2" type="reset">VIEW MORE STORIES</button>
+            </div>
+            <div className="footer">
+             <div className="fo1">
+             <div className="gg1">
+             <img src="./src/assets/image/12.png" alt="" className="a32a"></img>
+           <a className="a3a">Beautyness</a>
+           <img src="./src/assets/image/16.png" className="h12h"></img>
+           <img src="./src/assets/image/18.png" className="h13h"></img>
+           <img src="./src/assets/image/17.png" className="h14h"></img>
+           <img src="./src/assets/image/15.png" className="h15h"></img>
+             </div>
+              <p className="Line"></p>
+              <div className="gg2">
+                <div className="po1">
+                  <p className="pp1">Explore</p>
+                  <a className="pl1">Home</a>
+                  <a className="pl1">About Us</a>
+                  <a className="pl1">Services</a>
+                  <a className="pl1">Appointments</a>
+                  <a className="pl1">Blog</a>
+                  <a className="pl1">Contact Us</a>
+                </div>
+                <div className="po2">
+                <p className="pp1">Utility Pages</p>
+                  <a className="pl1">Start here</a>
+                  <a className="pl1">Style guide</a>
+                  <a className="pl1">404 not found</a>
+                  <a className="pl1">Password protected</a>
+                  <a className="pl1">Licenses</a>
+                  <a className="pl1">Changelog</a>
+                </div>
+                <div className="po3">
+                <p className="pp1">Keep in Touch</p>
+                  <a className="pl1">Address :24A Kingston St, Los Vegas NC 28202, USA.</a>
+                  <a className="pl1">Mail :support@doctors.com</a>
+                  <a className="pl1">Phone :(+22) 123 - 4567 - 900</a>
+                </div>
+                <div className="po4">
+                <p className="pp1">Working Hours</p>
+                  <a className="pl1">Mon to Fri: 7am - 6pm</a>
+                  <a className="pl1">Sat: 9am - 7pm</a>
+                  <a className="pl1">Sun: 9am - 6pm</a>
+                </div>
+              </div>
+             </div>
+              
+            </div>
+            <div className="foter2">
+            <a className="pl11">@ Copyright 2021. Drafted by Victor Themes.</a>
+            </div>
+
+            </div>
+          
+          </div>
+       
+    );
+};
